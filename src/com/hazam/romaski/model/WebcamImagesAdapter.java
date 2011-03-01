3ec84af8e44677cbded60b9218300771190d9480
hazam <emanuele.disaverio@gmail.com>
2011-03-01 23:04:28 +0100

    1.2

--- src/com/hazam/romaski/model/WebcamImagesAdapter.java
+++ src/com/hazam/romaski/model/WebcamImagesAdapter.java
@@ -8,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.Gallery;
 
 import com.hazam.romaski.R;
 import com.hazam.widget.RemoteImageView;
